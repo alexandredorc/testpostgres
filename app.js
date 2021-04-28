bda06c5bbc4fad183675a1e5fa3a853ff41f161d
alexandre dorcival <ddorcival@gmail.com>
2021-04-28 18:18:29 +0200

    set up express server

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-//importing modules
+var createError = require('http-errors');
 var express = require('express');
-var mongoose = require('mongoose');
-var bodyparser = require('body-parser');
-var cors = require('cors');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var registerRouter = require('./routes/register');
 
 var app = express();
 
-//port no
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-const port = 3000;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use('/', indexRouter);
+app.use('/register', registerRouter);
 
-app.use(bodyparser.json());
-
-app.use('/static',express.static(path.join(__dirname, 'public')));
-
-
-//testing
-app.get('/',(req, res)=>{
-	res.sendFile(path.join(__dirname,'/public/index.html'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/register',(req, res)=>{
-	res.sendFile(path.join(__dirname,'/public/test.html'));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.listen(port,()=>{
-	console.log('Server started at port: '+port);
-});
+module.exports = app;
